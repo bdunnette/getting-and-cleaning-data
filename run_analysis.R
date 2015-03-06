fe8fce2166a1067387bbe486cccea1b05072ee83
Brian Dunnette <brian@dunnette.us>
2015-03-06 12:35:56 -0600

    check to see if data is already downloaded; unzip to data directory

--- run_analysis.R
+++ run_analysis.R
@@ -5,8 +5,11 @@ if (!file.exists("data")) {
   dir.create("data")
 }
 
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileUrl, destfile="data/UCI_HAR_Dataset.zip", method="curl")
-print(list.files("./data"))
+if (!file.exists("data/UCI_HAR_Dataset.zip")) {
+  fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(fileUrl, destfile="data/UCI_HAR_Dataset.zip", method="curl")
+}
+
+unzip("data/UCI_HAR_Dataset.zip", exdir="data")
 
 
