612fa8e013a7f0bf37d568c8fb4e9d6f42b81bc3
Brian Dunnette <brian@dunnette.us>
2015-03-19 19:13:17 -0500

    adding comments & dataset names for course project

--- course-project/run_analysis.R
+++ course-project/run_analysis.R
@@ -1,15 +1,19 @@
+# set working directory to be the directory the script is located in
 this.dir <- dirname(parent.frame(2)$ofile)
 setwd(this.dir)
 
+# create data dir if it doesn't already exist
 if (!file.exists("data")) {
   dir.create("data")
 }
 
+# download zipped data files (using cURL, since it's HTTPS)
 if (!file.exists("data/UCI_HAR_Dataset.zip")) {
   fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
   download.file(fileUrl, destfile="data/UCI_HAR_Dataset.zip", method="curl")
 }
 
+# unzip data files to 'data' directory
 unzip("data/UCI_HAR_Dataset.zip", exdir="data")
 
-
+# write.table() using row.name=FALSE
