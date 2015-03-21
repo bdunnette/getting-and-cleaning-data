5f05b0fc9a8ef734973e923abd4a90aa211c99b6
Brian Dunnette <brian@dunnette.us>
2015-03-21 14:25:09 -0500

    merging and labeling activities for UCI HAR data

--- course-project/run_analysis.R
+++ course-project/run_analysis.R
@@ -1,6 +1,30 @@
+##########################################################################################################
+
+## Coursera Getting and Cleaning Data Course Project
+## Brian Dunnette
+## 2015-03-21
+
+# runAnalysis.r File Description:
+
+# This script will perform the following steps on the UCI HAR Dataset downloaded from 
+# https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+#
+# 1. Merge the training and the test sets to create one data set
+# 2. Extract only the measurements on the mean and standard deviation for each measurement 
+# 3. Use descriptive activity names to name the activities in the data set
+# 4. Appropriately label the data set with descriptive activity names 
+# 5. Create a second, independent tidy data set with the average of each variable for each activity and each subject
+
+##########################################################################################################
+
+# Clean up workspace
+rm(list=ls())
+
 # set working directory to be the directory the script is located in
 this.dir <- dirname(parent.frame(2)$ofile)
 setwd(this.dir)
+
+library(dplyr)
 
 # create data dir if it doesn't already exist
 if (!file.exists("data")) {
@@ -10,17 +34,43 @@ if (!file.exists("data")) {
 # download zipped data files (using cURL, since it's HTTPS)
 if (!file.exists("data/UCI_HAR_Dataset.zip")) {
   fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-  download.file(fileUrl, destfile="data/UCI_HAR_Dataset.zip", method="curl")
+  download.file(fileUrl, destfile="data/UCI_HAR_Dataset.zip", method="auto")
 }
 
-# unzip data files to 'data' directory
+# unzip data files to 'data' directory 
 unzip("data/UCI_HAR_Dataset.zip", exdir="data")
 
-# get column names from features.txt
-column_names <- read.table("data/UCI HAR Dataset/features.txt")
+# STEP 1: Merge the training and the test sets to create one data set
 
-# read data from both train and test datasets
-train_data <- read.table("data/UCI HAR Dataset/train/X_train.txt")
-test_data <- read.table("data/UCI HAR Dataset/test/X_test.txt")
+# get data column names from features.txt
+features <- read.table("data/UCI HAR Dataset/features.txt")
+
+# get activity info from activity_labels.txt
+activities <- read.table("data/UCI HAR Dataset/activity_labels.txt", col.names=c("activity_id", "activity_name"))
+
+# read data from both train and test datasets, setting column names from column 2 of features.txt
+x_train <- read.table("data/UCI HAR Dataset/train/X_train.txt", col.names=features[,2])
+x_test <- read.table("data/UCI HAR Dataset/test/X_test.txt", col.names=features[,2])
+
+# read activity IDs for both datasets
+y_train <- read.table("data/UCI HAR Dataset/train/y_train.txt", col.names=c("activity_id"))
+y_test <- read.table("data/UCI HAR Dataset/test/y_test.txt", col.names=c("activity_id"))
+
+# read subject IDs for both datasets
+subject_train <- read.table("data/UCI HAR Dataset/train/subject_train.txt", col.names=c("subject_id"))
+subject_test <- read.table("data/UCI HAR Dataset/test/subject_test.txt", col.names=c("subject_id"))
+
+# merge subject, activity, and accelerometer data for each dataset
+data_train <- cbind(dataset="train", subject_train, y_train, x_train)
+data_test <- cbind(dataset="test", subject_test, y_test, x_test)
+
+# merge both datasets
+data_merged <- rbind(data_train, data_test)
+
+# add activity labels
+data_labeled <- merge(activities, data_merged, by="activity_id")
+
+# use dplyr's tbl_df to make data frames more manageable
+data_tbl <- tbl_df(data_labeled)
 
 # write.table() using row.name=FALSE
