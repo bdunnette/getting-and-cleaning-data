d70c6d67e4f3d78c2d6146518572cd22fca38c8a
Brian Dunnette <bdunnette@gmail.com>
2015-03-29 11:50:22 -0500

    commenting week4/question5

--- week4/question5.R
+++ week4/question5.R
@@ -3,11 +3,24 @@
 
 library(quantmod)
 amzn <- getSymbols("AMZN",auto.assign=FALSE)
+
+#Get all AMZN values for 2012
 amzn_2012 <- amzn['2012']
 
+#Convert to a data frame for easier manipulation
 amzn_df <- as.data.frame(amzn_2012)
+
+#Get row names (dates) so that we can convert to days of the week
 row_names <- row.names(amzn_df)
+
+#Get day of week for each row using the weekdays() function
 row_names_weekdays <- lapply(row_names, function(date_str) weekdays(as.Date(date_str)))
+
+#Add day of week column to amzn_df data frame
 amzn_df$weekday <- row_names_weekdays
+
+#Create a logical vector based on whether each observation was on a Monday
 is_monday <- amzn_df$weekday == "Monday"
+
+#Finally, select only Monday observations
 amzn_mondays <- amzn_df[which(is_monday),]
